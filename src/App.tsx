cf293baca312d95f6a8fe0c5b072b7ddc7dae59c
tvue4 <143903098+tvue4@users.noreply.github.com>
2025-02-06 13:00:24 -0600

    Learned about padding and margins
    More daisyUI styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,24 @@ function App() {
     <>
       <div>
         <h1
-          className='text-2xl font-bold'
+          className='text-2xl font-bold bg-base-300 p-4 text-secondary'
         >
           TCA Catan
         </h1>
-        <button
-          className='btn btn-secondary btn-soft btn-xl'
+        <div 
+          className="p-4"
         >
-          Play Five Crowns
-        </button>
+          <button
+          className='btn btn-secondary btn-soft btn-xl p-4'
+          >
+            Play Five Crowns
+          </button>
+          <h2
+            className='mt-3 text-xl font-semi-bold'
+          >
+            Leaderboard...
+          </h2>
+        </div>
       </div>
     </>
   )
