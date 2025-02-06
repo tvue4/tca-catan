d21917bc8eb34d245e00479879574144aa1f3174
tvue4 <143903098+tvue4@users.noreply.github.com>
2025-02-06 13:12:44 -0600

    Learned my card ui-hammer
    Played around with "infinite" scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,160 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <div>
-        <h1
-          className='text-2xl font-bold bg-base-300 p-4 text-secondary'
-        >
+        <h1 className="text-2xl font-bold bg-base-300 p-4 text-secondary">
           TCA Catan
         </h1>
-        <div 
-          className="p-4"
-        >
-          <button
-          className='btn btn-secondary btn-soft btn-xl p-4'
-          >
+        <div className="p-4">
+          <button className="btn btn-secondary btn-soft btn-xl p-4">
             Play Five Crowns
           </button>
-          <h2
-            className='mt-3 text-xl font-semi-bold'
+          <h2 className="mt-3 text-xl font-semi-bold">Leaderboard...</h2>
+
+          <div 
+            className="card w-full bg-base-100 card-lg shadow-sm mt-4"
           >
-            Leaderboard...
-          </h2>
+            <div 
+              className="card-body"
+            >
+              <h2 
+                className="card-title"
+              >
+                  Leaderboard...
+              </h2>
+              <p>
+                Leaderboard coming soon...
+              </p>
+            </div>
+          </div>
+
+
+          <div 
+            className="card w-full bg-base-100 card-lg shadow-sm mt-4"
+          >
+            <div 
+              className="card-body"
+            >
+              <h2 
+                className="card-title"
+              >
+                  Leaderboard...
+              </h2>
+              <p>
+                Leaderboard coming soon...
+              </p>
+            </div>
+          </div>
+
+          
+
+
+          <div 
+            className="card w-full bg-base-100 card-lg shadow-sm mt-4"
+          >
+            <div 
+              className="card-body"
+            >
+              <h2 
+                className="card-title"
+              >
+                  Leaderboard...
+              </h2>
+              <p>
+                Leaderboard coming soon...
+              </p>
+            </div>
+          </div>
+
+
+
+
+
+
+          <div 
+            className="card w-full bg-base-100 card-lg shadow-sm mt-4"
+          >
+            <div 
+              className="card-body"
+            >
+              <h2 
+                className="card-title"
+              >
+                  Leaderboard...
+              </h2>
+              <p>
+                Leaderboard coming soon...
+              </p>
+            </div>
+          </div>
+
+
+
+
+          <div 
+            className="card w-full bg-base-100 card-lg shadow-sm mt-4"
+          >
+            <div 
+              className="card-body"
+            >
+              <h2 
+                className="card-title"
+              >
+                  Leaderboard...
+              </h2>
+              <p>
+                Leaderboard coming soon...
+              </p>
+            </div>
+          </div>
+
+
+
+
+          <div 
+            className="card w-full bg-base-100 card-lg shadow-sm mt-4"
+          >
+            <div 
+              className="card-body"
+            >
+              <h2 
+                className="card-title"
+              >
+                  Leaderboard...
+              </h2>
+              <p>
+                Leaderboard coming soon...
+              </p>
+            </div>
+          </div>
+
+
+
+
+          <div 
+            className="card w-full bg-base-100 card-lg shadow-sm mt-4"
+          >
+            <div 
+              className="card-body"
+            >
+              <h2 
+                className="card-title"
+              >
+                  Leaderboard...
+              </h2>
+              <p>
+                Leaderboard coming soon...
+              </p>
+            </div>
+          </div>
+
+
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
