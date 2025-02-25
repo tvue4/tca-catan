7a2b859a237bfab1e416190cdf099acc5b71d1ee
tvue4 <143903098+tvue4@users.noreply.github.com>
2025-02-25 12:44:07 -0600

    Showing the feature in JSX
    Need to hook up the plus button next

--- src/Play.tsx
+++ src/Play.tsx
@@ -11,6 +11,16 @@ export const Play = () => {
             >
                 Play
             </h3>
+            <h4 
+                className="text-lg font-semibold"
+            >
+                Turn #1
+                <button 
+                    className="btn btn-xs btn-outline btn-light ml-4"
+                >
+                    +
+                </button>    
+            </h4>
             <button className="btn btn-active btn-secondary btn-xl mt-4"
                 onClick={
                     () => nav(-2)
