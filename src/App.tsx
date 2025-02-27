f0b61233b7ec3fa0786bb76783e8a0874337138e
tvue4 <143903098+tvue4@users.noreply.github.com>
2025-02-27 13:47:55 -0600

    Leaderboard Donzos ! ! !
    Including CTA(call to action)

--- src/App.tsx
+++ src/App.tsx
@@ -68,7 +68,8 @@ const App = (
   // 
   // Hooks...
   // 
-  const [gameResults, setGameResults] = useState(dummyGameResults);
+  // const [gameResults, setGameResults] = useState<GameResult[]>(dummyGameResults);
+  const [gameResults, setGameResults] = useState<GameResult[]>([]);
 
   // 
   // Other (not hooks)
--- src/Home.tsx
+++ src/Home.tsx
@@ -42,54 +42,63 @@ export const Home: React.FC<HomeProps> = ({
                     >
                         Leaderboard
                     </h2>
-
-                    <div 
-                        className="overflow-x-auto"
-                    >
-                        <table 
-                            className="table"
-                        >
-                            {/* head */}
-                            <thead>
-                                <tr>
-                                    <th>
-                                        W
-                                    </th>
-                                    <th>
-                                        L
-                                    </th>
-                                    <th>
-                                        AVG
-                                    </th>
-                                    <th>
-                                        PLAYER
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    leaderboardData.map(
-                                        x => (
-                                            <tr>
-                                            <td>
-                                                {x.wins}
-                                            </td>
-                                            <td>
-                                                {x.losses}
-                                            </td>
-                                            <td>
-                                                {x.average}
-                                            </td>
-                                            <td>
-                                                {x.player}
-                                            </td>
+                    {
+                        leaderboardData.length > 0
+                            ? (
+                                <div 
+                                className="overflow-x-auto"
+                            >
+                                <table 
+                                    className="table"
+                                >
+                                    <thead>
+                                        <tr>
+                                            <th>
+                                                W
+                                            </th>
+                                            <th>
+                                                L
+                                            </th>
+                                            <th>
+                                                AVG
+                                            </th>
+                                            <th>
+                                                PLAYER
+                                            </th>
                                         </tr>
-                                        )
-                                    )
-                                }
-                            </tbody>
-                        </table>
-                    </div>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            leaderboardData.map(
+                                                x => (
+                                                    <tr>
+                                                    <td>
+                                                        {x.wins}
+                                                    </td>
+                                                    <td>
+                                                        {x.losses}
+                                                    </td>
+                                                    <td>
+                                                        {x.average}
+                                                    </td>
+                                                    <td>
+                                                        {x.player}
+                                                    </td>
+                                                </tr>
+                                                )
+                                            )
+                                        }
+                                    </tbody>
+                                </table>
+                            </div>
+                            )
+                            : (
+                                <p>
+                                    Play a game of Five Crowns to see the leaderboard ! ! !
+                                </p>
+                            )
+                    }
+                    
                 </div>
             </div>
         </>
