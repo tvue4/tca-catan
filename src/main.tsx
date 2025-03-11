bb9013063affbfd0dac2caf1e568d18b70310d04
tvue4 <143903098+tvue4@users.noreply.github.com>
2025-03-11 12:51:28 -0500

    Oops, these were problems
    Try again

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,6 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App 
-      timestamp={new Date().toISOString()}
-      magicNumber={42}
-    />
+    <App />
   </StrictMode>,
 )
