7c7da545af49d90ecf4e71d99b6d06d84cc65415
tvue4 <143903098+tvue4@users.noreply.github.com>
2025-04-29 13:30:30 -0500

    Changed AppTitle to "Catan Companion"

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router";
 import { GeneralFacts, LeaderboardEntry } from "./GameResults";
 import { useEffect } from "react";
 
-export const AppTitle = "Catan";
+export const AppTitle = "Catan Companion";
 
 interface HomeProps {
     leaderboardData: LeaderboardEntry[];
